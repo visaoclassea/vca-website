034ebccd6eaf27dbc53270d3aa97fea4f1cdb383
visaoclassea <contato@visaoclassea.com.br>
2026-07-31 15:45:09 -0300

    Modify sitemap function to add new URLs
    Updated sitemap function to include new URLs for 'consulta-classe-a' and 'agendamento'.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,12 @@
-export default function sitemap(){
-return[
-{url:"https://www.visaoclassea.com.br",priority:1},
-{url:"https://www.visaoclassea.com.br/servicos"},
-{url:"https://www.visaoclassea.com.br/empresas"},
-{url:"https://www.visaoclassea.com.br/sobre"},
-{url:"https://www.visaoclassea.com.br/contato"},
-{url:"https://www.visaoclassea.com.br/duvidas"},
-];
+export default function sitemap() {
+  return [
+    { url: "https://www.visaoclassea.com.br", priority: 1 },
+    { url: "https://www.visaoclassea.com.br/servicos" },
+    { url: "https://www.visaoclassea.com.br/consulta-classe-a" },
+    { url: "https://www.visaoclassea.com.br/agendamento" },
+    { url: "https://www.visaoclassea.com.br/empresas" },
+    { url: "https://www.visaoclassea.com.br/sobre" },
+    { url: "https://www.visaoclassea.com.br/contato" },
+    { url: "https://www.visaoclassea.com.br/duvidas" },
+  ];
 }
