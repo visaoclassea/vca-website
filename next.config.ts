db10b40b0269ad6f1a16b65ea60a80c4ca979acd
visaoclassea <contato@visaoclassea.com.br>
2026-07-31 14:39:38 -0300

    Add rewrites for specific host in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,22 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = { reactStrictMode: true };
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "consulta.visaoclassea.com.br",
+          },
+        ],
+        destination: "/consulta-classe-a",
+      },
+    ];
+  },
+};
+
 export default nextConfig;
